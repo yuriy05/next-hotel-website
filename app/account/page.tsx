36f63dd3ca7288cbac6bd91cfcc9d89f4e6e3d8a
yuriy05 <dilayyura@gmail.com>
2024-10-20 23:59:33 +0300

    file structure | routes | styling

---
+++ app/account/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Guest Area"
+};
+
+export default function Page() {
+  return <h1>Account page</h1>;
+}
---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-sm text-primary-700 text-center pb-1">
+      Copyright &copy; by The Wild Oasis
+    </footer>
+  );
+}
